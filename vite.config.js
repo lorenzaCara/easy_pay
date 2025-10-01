571bce30e6213b3a661638439c164298bae6714c
Lorenza Caramanico <locaraman@gmail.com>
2025-10-01 10:02:44 +0200

    edit file vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,4 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build',  
-        manifest: true,          
-        emptyOutDir: true,
-    },
 });
-
